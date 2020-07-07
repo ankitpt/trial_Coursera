52e7f94519f1f3b8021cc4310b77af97e52aaa1f
Ankit Patel <pate1019@purdue.edu>
2020-07-07 11:57:46 -0400

    New branch

--- example.py
+++ example.py
@@ -1,3 +1,3 @@
 def git_opeation():
- print("I am adding example.py file to the remote repository.")
+ print("I am adding example.py file to the remote repository in new branch feature1.")
 git_opeation()
