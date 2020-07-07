0ee7020edb7ed5ed8f584988b71a2fad7b74cec9
ankitpt <pate1019@purdue.edu>
2020-07-07 15:43:18 -0400

    pushing hope

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository in new branch feature1.")
  print("Also now pulling for making changes locally and push upstream")
+ print("Innocent change on feature1 branch. Will now try to push")
 git_opeation()
