d6ab95b42cf0e47426e836fa445ca1187585c30c
ankitpt <pate1019@purdue.edu>
2020-07-07 12:04:19 -0400

    Made change locally on feature1

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
 def git_opeation():
  print("I am adding example.py file to the remote repository in new branch feature1.")
+ print("Also now pulling for making changes locally and push upstream")
 git_opeation()
